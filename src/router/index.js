e9c501c15aa03c482899b4d28d4cf256041bd1be
Mac <mac@MacBook-Air-de-Mac.local>
2025-04-13 10:39:30 +0100

    Your commit message here

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import HomeView from '../views/HomeView.vue'
 import HomeUser from '../views/HomeUser.vue'
 import LoginView from '../views/LoginView.vue'
 import ProfileUser from '../views/ProfileUser.vue'
+import DiscussionView from '../views/SingleDiscussion.vue'
+import CreateThread from '../views/CreateDiscussion.vue'
+import DashboardView from '../views/DicussionsList.vue'
 
 const routes = [{
         path: '/',
@@ -15,6 +18,12 @@ const routes = [{
         component: LoginView
     },
     {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardView,
+        meta: { requiresAuth: true } 
+      },
+    {
         path: '/home',
         name: 'homeUser',
         component: HomeUser,
@@ -24,7 +33,20 @@ const routes = [{
         name: 'profileUser',
         component: ProfileUser,
         props: true
-    }
+    },
+    {
+        path: '/discussion/:id',
+        name: 'Discussion',
+        component: DiscussionView,
+        meta: { requiresAuth: true } // Authentication required for discussions
+      },
+      
+      {
+        path: '/create',
+        name: 'CreateThread',
+        component: CreateThread,
+        meta: { requiresAuth: true } // Authentication required for creating threads
+      }
 ]
 
 const router = createRouter({
